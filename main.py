7e4b866734f55e17bc8fbf20d37405197c966445
Igor Ubasev <Igor-Ubasev@yandex.ru>
2021-11-02 19:10:17 +0300

    Lesson_1

---
+++ main.py
@@ -0,0 +1,34 @@
+name = "Igor "
+print(type(name), name)
+
+a = 10
+print(type(a), a)
+
+b = 31.5
+print(type(b), b)
+
+c = bytes(300)
+print(type(c), c)
+
+d = ["Igor", "Lidia", "Sava"]
+print(type(d), d)
+
+e =  tuple ("Hello, Igor")
+print(type(e), e)
+
+f = {1, 2, 3, "Igor"}
+print(type(f), f)
+
+i =  frozenset ({"Hello Igor"})
+print(type(i), i)
+
+p = {"Russia": "Moscow", "Francia": "Paris", "Spain": "Madrid"}
+print(type(p), p)
+
+surname = "Ubasev"
+man = name + surname
+print(man)
+
+print(name, a)
+
+print(name + str(a))
